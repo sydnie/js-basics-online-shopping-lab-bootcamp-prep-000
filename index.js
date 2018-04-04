5e8aa6c1dbc63dffcfa60a94c3bdbe0876c86dae
Sydnie T <sydnietom@gmail.com>
2018-04-04 06:42:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry we don't have a credit card on file for you.";
   } else {
-    var cart = [];
+    cart = [];
     return `Your total cost is $${total()}, which will be charged to the card ${placeOrder()}.`;
   }
 }
