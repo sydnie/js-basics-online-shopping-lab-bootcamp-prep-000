7785b9cd538534afb0143f6cf16099ec037e5056
Sydnie T <sydnietom@gmail.com>
2018-04-04 06:45:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
       var currentItem = cart[i];
-      if (currentItem.itemName === item.itemName) {
+      if (currentItem === item) {
         cart.splice(i, 1);
         return cart;
       }
